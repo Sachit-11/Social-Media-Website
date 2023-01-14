6517b0267018e7f8d407c365cca9686117968ce9
Sachit Gupta <97462285+Sachit-11@users.noreply.github.com>
2023-01-14 11:31:09 +0530

    Adding Project

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,116 @@
+import "./rightBar.scss";
+
+const RightBar = () => {
+  return (
+    <div className = "rightBar">
+      <div className = "container">
+        <div className = "item">
+
+          <span>Suggestions For You</span>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <span>Sachit Gupta</span>
+            </div>
+            <div className = "buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <span>Sachit Gupta</span>
+            </div>
+            <div className = "buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <div className = "item">
+          <span>Latest Activities</span>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+                <p>
+                  <span> Sachit </span> changed his profile picture
+                </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <p>
+                <span>Sachit</span> changed his profile picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <p>
+                <span>Sachit</span> changed his profile picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <p>
+                <span>Sachit</span> changed his profile picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        
+        <div className="item">
+          <span>Online Friends</span>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <div className="online" />
+              <span>Sachit</span>
+            </div>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <div className="online" />
+              <span>Sachit</span>
+            </div>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <div className="online" />
+              <span>Sachit</span>
+            </div>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <div className="online" />
+              <span>Sachit</span>
+            </div>
+          </div>
+          <div className = "user">
+            <div className = "userInfo">
+              <img src = "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt = "" />
+              <div className="online" />
+              <span>Sachit</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RightBar;
